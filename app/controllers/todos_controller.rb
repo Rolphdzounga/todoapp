41bc4bae1c403ae2fd552f99434a2e91ed6e139d
Rolph Francel DZOUNGA <rolphfrancel@gmail.com>
2017-09-01 03:26:50 +0100

    Implémentation des maj des taches:ajout des actions edit, update et leur views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -21,6 +21,19 @@ class TodosController < ApplicationController
   	@todo = Todo.find params[:id]
   end
 
+  def edit
+  	@todo = Todo.find params[:id]
+  end
+
+  def update
+  	@todo = Todo.find params[:id] 
+  	if @todo.update todos_params
+  		flash[:notice] = "Mise à jour effectué avec succès..."
+  		redirect_to todo_path @todo
+  	else
+  		render 'edit'
+  	end
+  end
   private
 
   	def todos_params
